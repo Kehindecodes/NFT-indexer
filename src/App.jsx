620468b60193cc309d38cb22e347f1870d71a53a
Oki Kehinde <olamiposi.oki@gmail.com>
2023-05-10 14:30:33 +0100

    add image overlay

--- src/App.jsx
+++ src/App.jsx
@@ -240,6 +240,15 @@ function App() {
 									results.ownedNfts.map((e, i) => {
 										return (
 											<Box position='relative'>
+												<Box
+													position='absolute'
+													top={0}
+													right={0}
+													bottom={0}
+													left={0}
+													bg='rgba(0, 0, 0, 0.5)'
+													borderRadius={20}
+												/>
 												<Image
 													src={tokenDataObjects[i].rawMetadata.image}
 													alt='Example Image'
